041f5ce17d81bce6eed59708427c04c2e9bd34ab
0112navdeep1996singh <142915262+0112navdeep1996singh@users.noreply.github.com>
2023-12-02 17:31:41 +0530

    Add files via upload

---
+++ ShoppingCart1/src/main/java/com/shoppingcart/connection/DbConnection.java
@@ -0,0 +1,19 @@
+package com.shoppingcart.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+private static Connection connection = null;
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException { 
+		if(connection==null) {
+			Class.forName("com.mysql.jdbc.Driver");
+          connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_cart1","root","pass123");
+		  System.out.println("connected");
+		}
+		return connection;
+	}
+
+}
---
+++ ShoppingCart1/src/main/java/com/shoppingcart/servlet/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.shoppingcart.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shoppingcart.connection.DbConnection;
+import com.shoppingcart.dao.UserDao;
+import com.shoppingcart.model.User;
+
+
+@WebServlet("/user-login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("login.jsp");
+		
+	}
+
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		try (PrintWriter out = response.getWriter()) {
+			String email = request.getParameter("login-email");
+			String password = request.getParameter("login-password");
+
+			UserDao udao = new UserDao(DbConnection.getConnection());
+			User user = udao.userLogin(email, password);
+			if (user != null) {
+				request.getSession().setAttribute("auth", user);
+				System.out.print("user logged in");
+				response.sendRedirect("index.jsp");
+				
+			} else {
+				out.println("there is no user");
+			}
+
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
+		} 
+
+	}
+}
